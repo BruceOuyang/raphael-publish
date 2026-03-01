200cb5f7d73b017bf9768d84d293bcde66575eaf
Mark L <73659136+markliuyuxiang@users.noreply.github.com>
2026-03-02 00:02:06 +0800

    style: left-align preview panel by default

--- src/components/PreviewPanel.tsx
+++ src/components/PreviewPanel.tsx
@@ -9,7 +9,7 @@ interface PreviewPanelProps {
 export default function PreviewPanel({ renderedHtml, deviceWidthClass, previewRef }: PreviewPanelProps) {
     return (
         <div className="relative overflow-y-auto no-scrollbar bg-[#f2f2f7]/50 dark:bg-[#000000] pt-12 pb-32 block z-20 flex-1 min-h-0">
-            <div className={`bg-white rounded-[24px] overflow-hidden shadow-apple-lg ${deviceWidthClass} transition-all duration-500 ring-1 ring-[#00000008] border-t border-white/50 mx-auto h-fit min-h-[calc(100%-48px)]`}>
+            <div className={`bg-white rounded-[24px] overflow-hidden shadow-apple-lg ${deviceWidthClass} transition-all duration-500 ring-1 ring-[#00000008] border-t border-white/50 ml-4 md:ml-6 mr-auto h-fit min-h-[calc(100%-48px)]`}>
                 <div
                     ref={previewRef}
                     dangerouslySetInnerHTML={{ __html: renderedHtml }}
